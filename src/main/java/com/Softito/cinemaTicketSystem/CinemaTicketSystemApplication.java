02b3549131687e2c2090ec5be6f841f483c57520
Kadir UĞURLU <kadir--ugurlu@hotmail.com>
2023-04-27 20:48:18 +0300

    Spring Boot projesi oluşturuldu.

---
+++ src/main/java/com/Softito/cinemaTicketSystem/CinemaTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Softito.cinemaTicketSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketSystemApplication.class, args);
+	}
+
+}
